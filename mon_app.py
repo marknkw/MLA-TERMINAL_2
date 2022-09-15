259e07bf48122ee9db0db33a44ce391bf766efd9
marknkw <mark.s.g@hotmail.com>
2022-09-14 21:36:12 -0300

    Testing_CGI
    Print_HTML_Page

--- mon_app.py
+++ mon_app.py
@@ -4,3 +4,8 @@
 #Console.Beep(randomPitch, 140);
 #Thread.Sleep(beepPause);
 #Console.Beep(randomPitch + random.Next(-20, 50), 140);
+import cgi
+file = open('resources/monad.html', 'r+')
+file_lines = file.readlines()
+for i in file_lines:
+    print(i.)
