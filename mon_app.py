990ceeb4ce0a4b42e26d30abdb534aff5bccff9b
marknkw <37668487+marknkw@users.noreply.github.com>
2022-10-09 20:18:20 -0300

    Update_01
    Eyes

--- mon_app.py
+++ mon_app.py
@@ -4,8 +4,39 @@
 #Console.Beep(randomPitch, 140);
 #Thread.Sleep(beepPause);
 #Console.Beep(randomPitch + random.Next(-20, 50), 140);
-import cgi
-file = open('resources/monad.txt', 'r+')
-file_lines = file.readlines()
-for i in file_lines:
-    print(i)
+
+#Mostra sequência do olho piscando
+
+import time
+def read_eyes():
+    """
+    The read_eyes function reads in the three eye files and returns a list of lists.
+    The outer list contains 3 elements, each element is a list containing all lines from an eye file.
+    
+    
+    :return: A list of lists
+    """
+    with open('resources/eye_01.txt', 'r+') as file_1, \
+         open('resources/eye_02.txt', 'r+') as file_2,  \
+         open('resources/eye_03.txt', 'r+') as file_3:
+         list = []
+         eye_01 = file_1.readlines()
+         eye_02 = file_2.readlines()
+         eye_03 = file_3.readlines()
+         list.append(eye_01)
+         list.append(eye_02)
+         list.append(eye_03)
+         return list
+
+eyes = read_eyes()
+
+def olhoPiscando(eyes):
+    for i in eyes[0]:
+        print(i)
+    for i in eyes[1]:
+        print(i)
+    for i in eyes[2]:
+        print(i)
+
+olhoPiscando(eyes)
+
---
+++ ponicode/mon_app_test.py
@@ -0,0 +1,6 @@
+import mon_app
+
+class Test_Mon_app_Read_eyes:
+    def test_read_eyes_1(self):
+        result = mon_app.read_eyes()
+
