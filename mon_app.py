119143e4d04c61c33bee7f2167257947caf2ea14
marknkw <37668487+marknkw@users.noreply.github.com>
2022-10-10 23:39:24 -0300

    Update_Adds_Sound
    Adds sound

--- mon_app.py
+++ mon_app.py
@@ -16,8 +16,18 @@ import os
 
 import threading
 
-#Função que printa letra por letra de uma frase com delay
+
+# Função que printa letra por letra de uma frase com delay
+
+
 def delay_print(s):
+    """
+    The delay_print function prints the input string character by by, with a 100ms delay between each character.
+    This is helpful for simulating print statements that take time to execute.
+    
+    :param s: Pass the string that you want to print
+    :return: nothing
+    """
     for c in s:
         sys.stdout.write(c)
         sys.stdout.flush()
@@ -30,7 +40,7 @@ def clear():
     It is used to make the output of multiple function calls easier to read by 
     clearing the screen before each call.
 
-    :return: The output of the os
+    :return: terminal reset
     """
     return os.system('cls' if os.name == 'nt' else 'clear')
 
@@ -45,19 +55,42 @@ def readEyes():
     :return: A list of lists
     """
     with open('resources/eye_01.txt', 'r+') as file_1,\
-            open('resources/eye_02.txt', 'r+') as file_2,\
-            open('resources/eye_03.txt', 'r+') as file_3:
-        list = []
-        eye_01 = file_1.readlines()
-        eye_02 = file_2.readlines()
-        eye_03 = file_3.readlines()
-        file_1.close()
-        file_2.close()
-        file_3.close()
-        list.append(eye_01)
-        list.append(eye_02)
-        list.append(eye_03)
-        return list
+         open('resources/eye_02.txt', 'r+') as file_2,\
+         open('resources/eye_03.txt', 'r+') as file_3,\
+         open('resources/eye_04.txt', 'r+') as file_4,\
+         open('resources/eye_05.txt', 'r+') as file_5,\
+         open('resources/eye_06.txt', 'r+') as file_6,\
+         open('resources/eye_07.txt', 'r+') as file_7,\
+         open('resources/eye_08.txt', 'r+') as file_8:
+
+
+         list = []
+         eye_01 = file_1.readlines()
+         eye_02 = file_2.readlines()
+         eye_03 = file_3.readlines()
+         eye_04 = file_4.readlines()
+         eye_05 = file_5.readlines()
+         eye_06 = file_6.readlines()
+         eye_07 = file_7.readlines()
+         eye_08 = file_8.readlines()              
+        
+         file_1.close()
+         file_2.close()
+         file_3.close()
+         file_4.close()
+         file_5.close()
+         file_6.close()
+         file_7.close()
+         file_8.close()
+         list.append(eye_01)
+         list.append(eye_02)
+         list.append(eye_03)
+         list.append(eye_04)
+         list.append(eye_05)
+         list.append(eye_06)
+         list.append(eye_07)
+         list.append(eye_08)
+         return list
 
 
 def olhoPiscando(eyes):
@@ -72,24 +105,65 @@ def olhoPiscando(eyes):
     while True:
         for i in eyes[0]:
             print(i)
+        print("press-any-key")
         sleep(randint(2, 5))
         clear()
         for i in eyes[1]:
             print(i)
+        print("press-any-key")
         sleep(0.01)
-        winsound.Beep(randint(400, 450), 130)
         clear()
         for i in eyes[2]:
             print(i)
+        print("press-any-key")
         clear()
+        for i in eyes[3]:
+            print(i)
+        print("press-any-key")
+        sleep(randint(2, 5))
+        winsound.Beep(randint(400, 450), 130)
+        clear()
+        for i in eyes[4]:
+            print(i)
+        print("press-any-key")
+        sleep(0.5)
+        clear()
+        for i in eyes[5]:
+            print(i)
+        print("press-any-key")
+        sleep(0.5)
+        clear()
+        for i in eyes[6]:
+            print(i)
+        print("press-any-key")
+        sleep(0.5)
+        clear()
+        for i in eyes[7]:
+            print(i)
+        print("press-any-key")
+        sleep(randint(2, 5))
+        winsound.Beep(randint(400, 450), 130)
+        clear()
+                       
+        olhoPiscando(eyes)
+
 
 def boot():
+    """
+    The boot function is used to print the boot sequence of the game. It uses a for loop to iterate through each line in 
+    the text file and prints it with a delay between each character printed. The function also plays an audible tone at random 
+    frequencies between 400 and 450 Hz for 130 milliseconds after printing each line.
+
+    :return: nothing
+    """
     with open('resources/boot/boot.txt', 'r') as f:
-    # Comment: 
+        # Comment:
         for i in f:
             delay_print(i)
-            winsound.Beep(randint(400, 450), 130)
+            winsound.PlaySound("/resources/sounds/TerminalTypingUser.mp3", winsound.SND_ASYNC)
         # end open file)
+        delay_print("Loading...")
+
 
 boot()
 list = readEyes()
